296c06a0342b79e3a694f83f82341e60f30a8943
Filipe Covas <fcovas10@hotmail.com>
2021-11-17 01:27:36 +0000

    test(missig-test): missing test renders without crashing

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -5,7 +5,13 @@ import { SweetPasswordStrength } from '../src';
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<SweetPasswordStrength />, div);
+    ReactDOM.render(
+      <SweetPasswordStrength
+        password="12345"
+        onChange={PasswordStrenghtState => console.log(PasswordStrenghtState)}
+      />,
+      div
+    );
     ReactDOM.unmountComponentAtNode(div);
   });
 });
